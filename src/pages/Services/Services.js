07637045b9d9d3e50561d1484f6822c45ddd9522
Federico Marino <fede.m.fede@gmail.com>
2020-01-19 20:38:25 -0300

    icons on react

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,38 +1,45 @@
 import React from "react";
 import ServiceCard from "../../components/ServiceCard/ServiceCard";
 
+import AnalyticsIcon from './icons/analytics.svg';
+import BrowserIcon from './icons/browser.svg';
+import CodingIcon from './icons/coding.svg';
+import ConnectionIcon from './icons/connection.svg';
+import LayerIcon from './icons/layers.svg';
+import SeoIcon from './icons/seo-and-web.svg';
+
 import "./Services.scss";
 
 
 const Services = () =>{
     const services = [
         {
-            icon: "analytics.svg",
+            icon: AnalyticsIcon,
             title: "Analytics",
             body: "Im the best in analytics! I can offer you the best analytics stuff you will ever have and you will be surprised with the quality and test"
         },
         {
-            icon: "coding.svg",
+            icon: CodingIcon,
             title: "FrontEnd Development",
             body: "FrontEnd Development focused on the interactions and the design of the page for you or your clients to feel comfortable and engaged while navigating through the depths of the cthulhean sea, as far as the black skies allow us."
         },
         {
-            icon: "browser.svg",
+            icon: BrowserIcon,
             title: "Browser",
             body: "Browser is the way to go, dude!"
         },
         {
-            icon: "connection.svg",
+            icon: ConnectionIcon,
             title: "Connection",
             body: "Connection is the real stuff in connecting stuff"
         },
         {
-            icon: "layers.svg",
+            icon: LayerIcon,
             title: "Layers",
             body: "So many layers of complexity will win me money"
         },
         {
-            icon: "seo-and-web.svg",
+            icon: SeoIcon,
             title: "SEO and Web",
             body: "SEO is a tool. Web is a tool. I do both."
         }
